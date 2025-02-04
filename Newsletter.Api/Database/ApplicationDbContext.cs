2bc4e42b639d839491c110aeef069496fae3671f
Artem Vashchuk <artem.vashchuk@gmail.com>
2025-02-04 23:41:53 +0200

    DEV-0001: Create solution

---
+++ Newsletter.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newsletter.Api.Entities;
+
+namespace Newsletter.Api.Database;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Article>(builder =>
+            builder.OwnsOne(a => a.Tags, tagsBuilder => tagsBuilder.ToJson()));
+    }
+
+    public DbSet<Article> Articles { get; set; }
+}
